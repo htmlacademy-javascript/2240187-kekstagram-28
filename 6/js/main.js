851db85a31aeed50db2fd31f5a731e503cc4be3b
Keks <keks@htmlacademy.ru>
2023-03-10 18:33:36 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,9 @@
+import './util.js';
+import './data.js';
+import './createMiniatures.js';
+import {createPostDescription} from './data.js';
+import { createElement } from './createMiniatures.js';
+
+const FinalPost = createPostDescription();
+createElement(FinalPost);
+
