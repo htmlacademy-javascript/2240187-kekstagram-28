28625ed3c551dc4436ec1ed629b61fe6a5806d5b
Keks <keks@htmlacademy.ru>
2023-03-10 21:51:17 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/createMiniatures.js
+++ 6/js/createMiniatures.js
@@ -1,22 +1,21 @@
 
-const userDialog = document.querySelector('.big-picture');
-userDialog.classList.remove('hidden');
+//const userDialog = document.querySelector('.big-picture');
+//userDialog.classList.remove('hidden');
 
 
-const pictureContainer = userDialog.querySelector('.picture');
-const pictureElement = pictureContainer.querySelector('#picture');
+const pictureContainer = document.querySelector('.pictures');
+const pictureElement = document.querySelector('#picture').content.querySelector('.picture');
 
 
-const similarPhotoPosts = pictureElement.createDocumentFragment();
+const similarPhotoPosts = document.createDocumentFragment();
 
-export function createElement(randomPostData){
+export function createElement(randomPostData) {
   randomPostData.forEach((item) => {
     const postElement = pictureElement.cloneNode(true);
-    postElement.querySelector('.picture__img').src = item.url;
-    postElement.querySelector('.picture__comments').textContent = item.comments;
+    postElement.querySelector('.picture__img').src = item.urlAdress;
+    postElement.querySelector('.picture__comments').textContent = item.comment.length;
     postElement.querySelector('.picture__likes').textContent = item.likes;
     similarPhotoPosts.appendChild(postElement);
   });
+  pictureContainer.append(similarPhotoPosts);
 }
-pictureContainer.append(similarPhotoPosts);
-
