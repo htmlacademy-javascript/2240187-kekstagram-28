cf47f85464d9f8abb2eaddbd6b9c2936ac7e3dde
Keks <keks@htmlacademy.ru>
2023-04-06 17:48:27 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,26 @@
+import { renderThumbnail } from './renderThumbnail.js';
+import { renderBigPicture } from './renderBigPicture.js';
+import { renderUploadImage } from './renderUploadImage.js';
+import { getData } from './api.js';
+import { showAlert } from './util.js';
+import { setUserFormSubmit } from './validation.js';
+import { } from './sortPhotos.js';
+
+export function reRenderAll(data) {
+  renderThumbnail(data);
+  renderBigPicture(data);
+  renderUploadImage(data);
+}
+
+setUserFormSubmit();
+getData()
+  .then((response) => response.json())
+  .then((data) => {
+    reRenderAll(data);
+  })
+  .catch(
+    (err) => {
+      showAlert(err.message);
+    }
+  );
+
---
+++ 12/js/renderUploadImage.js
@@ -0,0 +1,72 @@
+import { isEscapeKey } from './util.js';
+import { scaleImageReset, scaleDefault, scaleValueControl, scaleBigger, scaleSmaller, buttonBigger, buttonSmaller } from './scale.js';
+import { imageUploadForm, hashtagInput, commentInput, pristine } from './validation.js';
+import { effectChange, effectReset } from './renderImageEffect.js';
+
+const uploadPhotoSection = document.querySelector('.img-upload__overlay');
+const imageUpload = document.querySelector('#upload-file');
+const cancelButton = document.querySelector('#upload-cancel');
+
+const onDocKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    closeImageEditor();
+  }
+};
+
+
+const inputInFocus = () => {
+  imageUpload.removeEventListener('keydown', onDocKeydown);
+};
+
+const inputOutFocus = () => {
+  imageUpload.addEventListener('keydown', onDocKeydown);
+};
+
+const addInputListener = () => {
+  hashtagInput.addEventListener('focus', inputInFocus);
+  commentInput.addEventListener('focus', inputInFocus);
+  hashtagInput.addEventListener('blur', inputOutFocus);
+  commentInput.addEventListener('blur', inputOutFocus);
+};
+
+const removeInputListener = () => {
+  hashtagInput.removeEventListener('focus', inputInFocus);
+  commentInput.removeEventListener('focus', inputInFocus);
+  hashtagInput.removeEventListener('blur', inputOutFocus);
+  commentInput.removeEventListener('blur', inputOutFocus);
+};
+
+export function closeImageEditor() {
+  uploadPhotoSection.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  imageUploadForm.reset();
+  pristine.reset();
+  scaleImageReset();
+  effectReset();
+  removeInputListener();
+
+  scaleSmaller.removeEventListener('click', buttonSmaller);
+  scaleBigger.removeEventListener('click', buttonBigger);
+  cancelButton.removeEventListener('click', closeImageEditor);
+  imageUploadForm.removeEventListener('change', effectChange);
+  imageUploadForm.removeEventListener('keydown', onDocKeydown);
+}
+
+function openImageEditor() {
+  uploadPhotoSection.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  scaleValueControl.value = `${scaleDefault}%`;
+  addInputListener();
+
+  scaleSmaller.addEventListener('click', buttonSmaller);
+  scaleBigger.addEventListener('click', buttonBigger);
+  cancelButton.addEventListener('click', closeImageEditor);
+  imageUploadForm.addEventListener('change', effectChange);
+  imageUploadForm.addEventListener('keydown', onDocKeydown);
+}
+
+const renderUploadImage = () => {
+  imageUpload.addEventListener('change', openImageEditor);
+};
+
+export {renderUploadImage};
